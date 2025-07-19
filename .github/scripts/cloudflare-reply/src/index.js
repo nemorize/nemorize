41540ea7301e3142de3aa02f61f3bb0cab284b0a
Ji Yong Kim <jiyong.kim@headercat.com>
2025-07-19 13:25:28 +0900

    feat: rewrite everything

---
+++ .github/scripts/cloudflare-reply/src/index.js
@@ -0,0 +1,196 @@
+// noinspection JSUnresolvedReference,JSUnusedGlobalSymbols,JSUnusedLocalSymbols
+
+import jwt from '@tsndr/cloudflare-worker-jwt'
+import { parse } from 'cookie'
+
+import badRequestHtml from './bad-request.html'
+import authFailedHtml from './auth-failed.html'
+import rateLimitHtml from './rate-limit.html'
+import noContentHtml from './no-content.html'
+import badFormHtml from './bad-form.html'
+import unAuthFormHtml from './unauth-form.html'
+import authFormHtml from './auth-form.html'
+
+export default {
+	async fetch(request, env, ctx) {
+		const { pathname, searchParams } = new URL(request.url)
+
+		if (request.method === 'GET' && pathname === '/auth') {
+			const path = searchParams.get('path')
+			if (!path) {
+				return new Response(String(badRequestHtml), {
+					status: 400,
+					headers: {
+						'content-type': 'text/html; charset=utf-8',
+					}
+				})
+			}
+			return Response.redirect(
+				`https://github.com/login/oauth/authorize?client_id=${env.GITHUB_CLIENT_ID}&state=${path}`,
+				302
+			)
+		}
+		if (request.method === 'GET' && pathname === '/callback') {
+			const code = searchParams.get('code')
+			const path = searchParams.get('state')
+			if (!code || !path) {
+				return new Response(String(badRequestHtml), {
+					status: 400,
+					headers: {
+						'content-type': 'text/html; charset=utf-8',
+					}
+				})
+			}
+
+			try {
+				const response0 = await fetch('https://github.com/login/oauth/access_token', {
+					method: 'POST',
+					headers: {
+						'content-type': 'application/json',
+						'accept': 'application/json',
+					},
+					body: JSON.stringify({
+						client_id: env.GITHUB_CLIENT_ID,
+						client_secret: env.GITHUB_CLIENT_SECRET,
+						code,
+					})
+				})
+				const output0 = await response0.json()
+				if (output0.error) {
+					return new Response(String(authFailedHtml), {
+						status: 400,
+						headers: {
+							'content-type': 'text/html; charset=utf-8',
+						}
+					})
+				}
+
+				const response1 = await fetch('https://api.github.com/user', {
+					method: 'GET',
+					headers: {
+						'user-agent': 'nemorize-reply-app',
+						'authorization': `Bearer ${output0.access_token}`,
+					},
+				})
+				const output1 = await response1.json()
+				if (output1.error) {
+					return new Response(String(authFailedHtml), {
+						status: 400,
+						headers: {
+							'content-type': 'text/html; charset=utf-8',
+						}
+					})
+				}
+
+				const token = await jwt.sign({
+					iss: env.BLOG_HOST,
+					sub: output1.login,
+				}, env.JWT_SECRET_KEY)
+				return new Response('', {
+					status: 302,
+					headers: {
+						'location': `https://${env.BLOG_HOST}${path}`,
+						'set-cookie': `auth-token=${token}; HttpOnly; SameSite=Lax; Secure`
+					}
+				})
+			} catch (e) {
+				return new Response(e.message, { status: 500 })
+			}
+		}
+		if (request.method === 'GET' && pathname === '/') {
+			const path = searchParams.get('path')
+			if (!path) {
+				return new Response(String(badFormHtml), {
+					status: 400,
+					headers: {
+						'content-type': 'text/html; charset=utf-8',
+					}
+				})
+			}
+			const cookie = parse(request.headers.get('cookie') || '')
+			if (cookie['auth-token']) {
+				const verified = await jwt.verify(cookie['auth-token'], env.JWT_SECRET_KEY)
+				if (verified && verified.payload.iss === env.BLOG_HOST && verified.payload.sub) {
+					return new Response(
+						String(authFormHtml)
+							.replaceAll('{{ path }}', path)
+							.replaceAll('{{ username }}', verified.payload.sub),
+						{
+							status: 200,
+							headers: {
+								'content-type': 'text/html; charset=utf-8',
+							}
+						}
+					)
+				}
+			}
+			return new Response(
+				String(unAuthFormHtml)
+					.replaceAll('{{ path }}', path),
+				{
+					status: 403,
+					headers: {
+						'content-type': 'text/html; charset=utf-8',
+					}
+				}
+			)
+		}
+		if (request.method === 'POST' && pathname === '/submit') {
+			const { success } = await env.RATE_LIMIT.limit({ key: '' })
+			if (!success) {
+				return new Response(String(rateLimitHtml), {
+					status: 429,
+					headers: {
+						'content-type': 'text/html; charset=utf-8',
+					}
+				})
+			}
+
+			const body = await request.formData()
+			const path = body.get('path')
+			if (!path || path.includes('.')) {
+				return new Response(String(badRequestHtml), {
+					status: 400,
+					headers: {
+						'content-type': 'text/html; charset=utf-8',
+					}
+				})
+			}
+			const content = body.get('content')?.trim()?.substring(0, 2000) || ''
+			if (!content) {
+				return new Response(String(noContentHtml), {
+					status: 400,
+					headers: {
+						'content-type': 'text/html; charset=utf-8',
+					}
+				})
+			}
+			const cookie = parse(request.headers.get('cookie') || '')
+			if (cookie['auth-token']) {
+				const verified = await jwt.verify(cookie['auth-token'], env.JWT_SECRET_KEY)
+				if (verified && verified.payload.iss === env.BLOG_HOST && verified.payload.sub) {
+					fetch(`https://api.github.com/repos/${GITHUB_WORKFLOW_REPO}/actions/workflows/${GITHUB_WORKFLOW_ID}/dispatches`, {
+						method: 'POST',
+						headers: {
+							'accept': 'application/vnd.github+json',
+							'authorization': `Bearer ${env.GITHUB_WORKFLOW_TOKEN}`,
+							'x-github-api-version': '2022-11-28'
+						},
+						body: JSON.stringify({
+							path,
+							content,
+							username: verified.payload.sub,
+						}),
+					}).then().catch()
+					return Response.redirect(`https://${env.BLOG_HOST}${path}`, 302)
+				}
+			}
+			return new Response(String(badRequestHtml), {
+				status: 400,
+				headers: {
+					'content-type': 'text/html; charset=utf-8',
+				}
+			})
+		}
+	},
+};
---
+++ .github/scripts/commit-reply/index.js
@@ -0,0 +1,42 @@
+import { format } from 'date-fns'
+import { TZDate } from '@date-fns/tz'
+import { readFile, writeFile } from 'fs/promises'
+
+const insertAfter = '<div id="comments"></div>'
+const commentTemplate = `
+      <div class="post__comment-item">
+        <div class="post__comment-item-header">
+          <img src="https://github.com/{{ username }}.png" alt="{{ username }}" />
+          <span>{{ username }}</span>
+          <time>${format(TZDate.tz('Asia/Seoul'), 'yyyy-MM-dd HH:mm')}</time>
+        </div>
+        <div class="post__comment-item-body">
+          {{ content }}
+        </div>
+      </div>
+`
+
+function escape (text) {
+  return text
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&#039;')
+}
+
+function build (username, content) {
+  return commentTemplate
+    .replaceAll('{{ username }}', escape(username))
+    .replaceAll('{{ content }}', escape(content))
+}
+
+const raw = process.env.json
+const inputs = JSON.parse(raw)
+
+let content = (await readFile(inputs.path + '/index.html')).toString()
+content = content.replace(
+  insertAfter,
+  insertAfter + build(inputs.username, inputs.content)
+)
+await writeFile(inputs.path + '/index.html', content)
